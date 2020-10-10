1e171d4e2681f860e98be24e8d77f5fd827f2cf5
SAGH <42285767+8sagh8@users.noreply.github.com>
2020-10-09 21:46:41 -0400

    c

--- js/level1script.js
+++ js/level1script.js
@@ -1,104 +1,93 @@
 /************* Time logic********************* */
 const spantime = document.querySelector("#ttime");
 
-let timer= parseInt(spantime.innerHTML);
+let timer = parseInt(spantime.innerHTML);
 let intervalRef;
 
-const countDown = () =>
-{
-  timer--;
+const countDown = () => {
+    timer--;
 
-  spantime.innerHTML = timer;
+    spantime.innerHTML = timer;
 
-  if(timer<=0)
-  {
-    clearInterval(intervalRef);
-    window.location.href='level2.html';
-  }
+    if (timer <= 0) {
+        clearInterval(intervalRef);
+        window.location.href = 'level2.html';
+    }
 }
 intervalRef = setInterval(countDown, 1000);
 
 
 /**************Random Numbers Calculations*************** */
 
-  /***initializing Correct Answer Variable as Global***/
-  let answerValue = 0;
-   /*****all containers****/
-  
-  let oneValue = document.querySelector("#valueOne");
-  let twoValue = document.querySelector("#valueTwo");
+/***initializing Correct Answer Variable as Global***/
+let answerValue = 0;
+/*****all containers****/
 
-  let oneOption = document.querySelector("#optionOne");
-  let twoOption = document.querySelector("#optionTwo");
-  let threeOption = document.querySelector("#optionThree");
-  let fourOption = document.querySelector("#optionFour");
-/*  let fiveOption = document.querySelector("#optionFive");
-  let sixOption = document.querySelector("#optionSix");
-  let sevenOption = document.querySelector("#optionSeven");
-  let eightOption = document.querySelector("#optionEight");
-  let nineOption = document.querySelector("#optionNine");
-*/
+let oneValue = document.querySelector("#valueOne");
+let twoValue = document.querySelector("#valueTwo");
 
-  /***Random Question and Answer Function */
-const randomQuestionNumbers=()=>
-{  
-  /*****all containers****/
+let oneOption = document.querySelector("#optionOne");
+let twoOption = document.querySelector("#optionTwo");
+let threeOption = document.querySelector("#optionThree");
+let fourOption = document.querySelector("#optionFour");
 
-  let valueO = Math.floor(Math.random()*6);
-  let valueT = Math.floor(Math.random()*6);
-  
+
+/***Random Question and Answer Function */
+const randomQuestionNumbers = () => {
+    /*****all containers****/
+
+    let valueO = Math.floor(Math.random() * 6);
+    let valueT = Math.floor(Math.random() * 6);
+
     /******Calulations****/
-  oneValue.innerHTML = valueO;
-  twoValue.innerHTML = valueT;
+    oneValue.innerHTML = valueO;
+    twoValue.innerHTML = valueT;
 
-  answerValue = valueO + valueT;
+    answerValue = valueO + valueT;
 
 
-/*************kill Duplicate Random Numbers****************/
+    /*************kill Duplicate Random Numbers****************/
 
-  let randomNumArray = [];
+    let randomNumArray = [];
 
-  for (let i = 0; i < 3; i++)
-    {
-      let valueLand = Math.floor(Math.random()*11);
-      randomNumArray.push(`${valueLand}`);
+    for (let i = 0; i < 3; i++) {
+        let valueLand = Math.floor(Math.random() * 11);
+        randomNumArray.push(`${valueLand}`);
 
-      console.log(randomNumArray[i]);
+        console.log(randomNumArray[i]);
 
-      let j = i - 1;
+        let j = i - 1;
 
-/********compare index "0" with correct answer*********** */
-        while (randomNumArray[0] == answerValue)
-        {
-          randomNumArray.pop();
-          let valueNewL = Math.floor(Math.random()*10);
-          randomNumArray.push(`${valueNewL}`);
-        }
-/*********compare all arrays num************************ */
-        for (j; j>=0; j--){
-        console.log(`for ==> i = ${i} j = ${j}`);
-          while(randomNumArray[i] == randomNumArray[j] || randomNumArray[i] == answerValue)
-          {
-            console.log(`while ==> i = ${i} j = ${j}`);
-
-            console.log(`before pop ${randomNumArray[i]}`);
+        /********compare index "0" with correct answer*********** */
+        while (randomNumArray[0] == answerValue) {
             randomNumArray.pop();
-           console.log(`before pop ${randomNumArray[i]}`);
-            let valueNewL = Math.floor(Math.random()*11);
+            let valueNewL = Math.floor(Math.random() * 10);
             randomNumArray.push(`${valueNewL}`);
-          console.log(`after push ${randomNumArray[i]}`);
-          }  
+        }
+        /*********compare all arrays num************************ */
+        for (j; j >= 0; j--) {
+            console.log(`for ==> i = ${i} j = ${j}`);
+            while (randomNumArray[i] == randomNumArray[j] || randomNumArray[i] == answerValue) {
+                console.log(`while ==> i = ${i} j = ${j}`);
+
+                console.log(`before pop ${randomNumArray[i]}`);
+                randomNumArray.pop();
+                console.log(`before pop ${randomNumArray[i]}`);
+                let valueNewL = Math.floor(Math.random() * 11);
+                randomNumArray.push(`${valueNewL}`);
+                console.log(`after push ${randomNumArray[i]}`);
+            }
         }
     }
 
 
     /*****Insert numbers at HTML**********/
 
-  oneOption.innerHTML = randomNumArray[0];
-  twoOption.innerHTML = randomNumArray[1];
-  threeOption.innerHTML = randomNumArray[2];
-  fourOption.innerHTML = answerValue;
-  /*fiveOption.innerHTML = randomNumArray[3];
+    oneOption.innerHTML = randomNumArray[0];
+    twoOption.innerHTML = randomNumArray[1];
+    threeOption.innerHTML = randomNumArray[2];
+    fourOption.innerHTML = answerValue;
+    /*fiveOption.innerHTML = randomNumArray[3];
   sixOption.innerHTML = randomNumArray[4];
   sevenOption.innerHTML = randomNumArray[5];
   eightOption.innerHTML = randomNumArray[6];
@@ -108,11 +97,11 @@ const randomQuestionNumbers=()=>
 
 
 /********AddEventListener*************/
-  window.addEventListener("load", randomQuestionNumbers);
-  oneOption.addEventListener("click", randomQuestionNumbers);
-  twoOption.addEventListener("click", randomQuestionNumbers);
-  threeOption.addEventListener("click", randomQuestionNumbers);
-  fourOption.addEventListener("click", randomQuestionNumbers);
+window.addEventListener("load", randomQuestionNumbers);
+oneOption.addEventListener("click", randomQuestionNumbers);
+twoOption.addEventListener("click", randomQuestionNumbers);
+threeOption.addEventListener("click", randomQuestionNumbers);
+fourOption.addEventListener("click", randomQuestionNumbers);
 /*  fiveOption.addEventListener("click", randomQuestionNumbers);
   sixOption.addEventListener("click", randomQuestionNumbers);
   sevenOption.addEventListener("click", randomQuestionNumbers);
@@ -132,22 +121,21 @@ let totalHits = 0;
 
 let spanHits = document.querySelector("#hitsSpan");
 
-const hitCalculation=()=>
-{
+const hitCalculation = () => {
 
-  totalHits += 1;
-  spanHits.innerHTML = totalHits;
+    totalHits += 1;
+    spanHits.innerHTML = totalHits;
 
-  let scoretoUpdate = parseInt(document.querySelector("#scoreSpan").innerHTML);
+    let scoretoUpdate = parseInt(document.querySelector("#scoreSpan").innerHTML);
 
-  scoretoUpdate += 100;
+    scoretoUpdate += 100;
 
-  scoreSpan.innerHTML = scoretoUpdate; 
+    scoreSpan.innerHTML = scoretoUpdate;
 
-  //LOCAL STORAGE
+    //LOCAL STORAGE
 
-  localStorage.fullScore = scoretoUpdate;
-  localStorage.fullHits = totalHits;
+    localStorage.fullScore = scoretoUpdate;
+    localStorage.fullHits = totalHits;
 
 }
 
@@ -158,30 +146,26 @@ fourOption.addEventListener("click", hitCalculation);
 
 let spanMiss = document.querySelector("#missSpan");
 
-const missCalculation=()=>
-{
-  totalMiss = totalMiss + 1;
+const missCalculation = () => {
+    totalMiss = totalMiss + 1;
 
-  if ( totalMiss == 6)
-  {
-    window.location.href=`report.html`;
-  }
+    if (totalMiss == 6) {
+        window.location.href = `report.html`;
+    } else spanMiss.innerHTML = totalMiss;
 
-  else spanMiss.innerHTML = totalMiss;
+    let scoretoUpdate = parseInt(document.querySelector("#scoreSpan").innerHTML);
 
-  let scoretoUpdate = parseInt(document.querySelector("#scoreSpan").innerHTML);
+    if (scoretoUpdate > 0)
+        scoretoUpdate -= 100;
 
-  if (scoretoUpdate > 0)
-  scoretoUpdate -= 100;
+    else scoretoUpdate = 0;
 
-  else scoretoUpdate = 0;
-  
-  scoreSpan.innerHTML = scoretoUpdate; 
+    scoreSpan.innerHTML = scoretoUpdate;
 
     //LOCAL STORAGE
 
-  localStorage.fullScore = scoretoUpdate;
-  localStorage.fullMisses = totalMiss;
+    localStorage.fullScore = scoretoUpdate;
+    localStorage.fullMisses = totalMiss;
 }
 
 oneOption.addEventListener("click", missCalculation);
